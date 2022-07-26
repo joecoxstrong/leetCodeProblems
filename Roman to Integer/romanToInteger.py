663f9f3afc9e434cca863cdf5e1cf9457f767bc3
joecoxstrong <joecoxstrong@gmail.com>
2022-07-26 16:36:47 -0400

    removed individual if/elif statements that were looking for 'iv' and 'ix' + created a .replace to look for and replace all instances where a roman numeral is used to subtract from a higher number + converted the roman numerals into a list + converted the roman numerals into numbers + print the sum of numbers

--- Roman to Integer/romanToInteger.py
+++ Roman to Integer/romanToInteger.py
@@ -14,38 +14,26 @@
     # 
     # The first thing that I am working on is taking an input and separating the letters into individual items in a list  
 def romanToInt():
-    roman_num = input('Enter a roman numeral to convert: ')
-    four='iv'
-    nine='ix'
-    # new_list = [letters for letters in roman_num]
-    # new_list[i] = new_list[i].capitalize()
-    # print(new_list)
-    # return (new_list)    
-    if four in roman_num:
-        num_with_four=roman_num.replace('iv','iiii')
-        list_with_four = [letters for letters in num_with_four]
-        for i in range(len(list_with_four)):
-            if list_with_four[i] == 'i':
-                list_with_four[i] = 1
-            elif list_with_four[i] == 'v':
-                list_with_four[i] = 5    
-            elif list_with_four[i] == 'x':
-                list_with_four[i] = 10
-            elif list_with_four[i] == 'l':
-                list_with_four[i] = 50
-            elif list_with_four[i] == 'c':
-                list_with_four[i] = 100
-            elif list_with_four[i] == 'd':
-                list_with_four[i] = 500
-            elif list_with_four[i] == 'm':
-                list_with_four[i] = 1000                         
-        
-        print(sum(list_with_four))
-    elif nine in roman_num:
-        num_with_nine=roman_num.replace('ix','viiii')
-        print(int(num_with_nine))
-        
-    else:
-        print('Is this your roman numeral? ',roman_num)    
+    user_roman_num = input('Enter a roman numeral to convert: ').upper()
+   
+    roman_num=user_roman_num.replace('IV','IIII').replace('IX','VIIII').replace('XL','XXXX').replace('XC','LXXXX').replace('CD','CCCC').replace('CM','DCCCC')    
+    list_of_roman_nums=[letters for letters in roman_num]
+    for i in range(len(list_of_roman_nums)):
+        if list_of_roman_nums [i] == 'I':
+            list_of_roman_nums [i] = 1
+        elif list_of_roman_nums [i] == 'V':
+            list_of_roman_nums [i] = 5    
+        elif list_of_roman_nums [i] == 'X':
+            list_of_roman_nums [i] = 10
+        elif list_of_roman_nums [i] == 'L':
+            list_of_roman_nums [i] = 50
+        elif list_of_roman_nums [i] == 'C':
+            list_of_roman_nums [i] = 100
+        elif list_of_roman_nums [i] == 'D':
+            list_of_roman_nums [i] = 500
+        elif list_of_roman_nums [i] == 'M':
+            list_of_roman_nums [i] = 1000 
+    print(sum(list_of_roman_nums))
+    
 
 romanToInt()        
